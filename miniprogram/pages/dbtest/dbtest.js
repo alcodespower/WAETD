efaec7df54e9128b58b751121c46f905003d4d7a
alcodespower <wzysws@gmail.com>
2020-04-27 00:27:28 +0800

    erwe
    wer

--- miniprogram/pages/dbtest/dbtest.js
+++ miniprogram/pages/dbtest/dbtest.js
@@ -5,8 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
+    sign:[],
+    resmessage:''
 
   },
+  formsubmit:function(data){
+    console.log(data.detail.value)
+    this.setData({
+      sign: data.detail.value
+    })
+    console.log(this.data.sign)
+    var username = data.detail.value.username
+    var phone = data.detail.value.phone
+    var password = data.detail.value.password
+    var password_again = data.detail.value.password_again
+    var face_url = data.detail.value.face_url;
+    wx.request({
+      url: 'https://php.iweb365.top/index.php/Home/User/sign' ,
+      method: 'POST',
+      dataType: 'json',
+      data:this.data.sign,
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' //修改此处即可
+      },
+      success: res => {
+        console.log(res)
+        this.setData({
+          resmessage:res.data.msg
+        })
+      },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  
 
   /**
    * 生命周期函数--监听页面加载
@@ -19,6 +51,8 @@ Page({
     //   },
     //   fail: console.error
     // })
+
+
 
   },
 
